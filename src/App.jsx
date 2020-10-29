b45e756de211c619ce7103e052d6b473544ac7e2
myselfrk <63996720+myselfrk@users.noreply.github.com>
2020-10-29 19:23:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 data-ns-test="project-name">NewWayBarber</h1>
+      <h2 data-ns-test="project-description">
+        A new way to book an appointment with your favorite barber.
+      </h2>
+    </div>
+  );
+}
